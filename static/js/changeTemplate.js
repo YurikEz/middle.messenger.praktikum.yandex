50ba5a4349f179f76fef05c4a7325b32fcc2c649
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-08-24 13:55:32 +0300

    Работа над чатом
    - Убрал роутинг через js
    - Сделал нативный роутинг через ссылки

--- static/js/changeTemplate.js
+++
@@ -1,8 +0,0 @@
-// TODO: Плохой подхож, исправить при написании более продвинутого роутинга
-/**
- * Переключаем шаблоны
- * @param root
- * @param template
- * @returns {*}
- */
-export default (root, template) => root.innerHTML = template;
--- static/js/miniRouter.js
+++
@@ -1,8 +0,0 @@
-import pages from '../../src/pages';
-
-/**
- * Получаем шаблон страницы
- * @param currentPage
- * @returns {*}
- */
-export default (currentPage) => pages[currentPage] || pages['_404'];
