95a070e5308cd6ad883aaf9bc489c5e24ac16966
Елманов Юрий Алексеевич <yuelmanov@phoenixit.ru>
2021-10-15 21:19:27 +0300

    upd server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '/dist/')));
 
-app.get('/*', function (req, res) {
+app.use('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
